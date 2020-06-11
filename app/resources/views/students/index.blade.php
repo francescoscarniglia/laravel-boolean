8a6d73dbd01cf8b84c33e354a4be8b3354a89d98
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-11 21:32:32 +0200

    Student info
    add array in array

--- app/resources/views/students/index.blade.php
+++ app/resources/views/students/index.blade.php
@@ -3,8 +3,7 @@
 @section('main-content')
 
 <h1>Studenti.</h1>
-<p>
-  Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.
-</p>
+
+@dump($students)
 
 @endsection
