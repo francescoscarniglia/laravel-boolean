6ae68e1b0d33cf279a9d39113f10d2d960050bfc
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-11 17:11:25 +0200

    add faq and privacy

---
+++ app/resources/views/static-pages/faq.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('main-content')
+
+<h1>Domande frequenti.</h1>
+<p>
+  Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.
+</p>
+
+@endsection
