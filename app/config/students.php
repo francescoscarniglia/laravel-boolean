9d16993d501c1b3c000c0fdd87019aceadc6f0dc
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-12 17:10:57 +0200

    Continue ex.

---
+++ app/config/students.php
@@ -0,0 +1,43 @@
+<?php
+return [
+  [
+    'id' => 1,
+    'img' => 'https://www.boolean.careers/images/students/biagini.png',
+    'nome' => 'Alessandro Biagini',
+    'eta' => 25,
+    'genere' => 'm',
+    'azienda' => 'DISC SPA',
+    'ruolo' => 'web developer',
+    'descrizione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
+  ],
+  [
+    'id' => 2,
+    'img' => 'https://www.boolean.careers/images/students/poggini.png',
+    'nome' => 'Paola Poggini',
+    'eta' => 24,
+    'genere' => 'f',
+    'azienda' => 'Prima Assicurazioni',
+    'ruolo' => 'junior software engineer',
+    'descrizione' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.'
+  ],
+  [
+    'id' => 3,
+    'img' => 'https://www.boolean.careers/images/students/masetti.png',
+    'nome' => 'Loana Masetti',
+    'eta' => 36,
+    'genere' => 'f',
+    'azienda' => 'The Zen Agency',
+    'ruolo' => 'web developer',
+    'descrizione' => 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.'
+  ],
+  [
+    'id' => 4,
+    'img' => 'https://www.boolean.careers/images/students/patruno.png',
+    'nome' => 'Simone Quarticelli',
+    'eta' => 32,
+    'genere' => 'm',
+    'azienda' => 'VLC 2',
+    'ruolo' => 'web developer',
+    'descrizione' => 'Per supportare la famiglia, Simone inizia a lavorare come operaio da giovanissimo. Fortemente motivato a cambiare vita, per 6 mesi non stacca mai lo sguardo dal traguardo, distinguendosi per tenacia e resilienza.'
+  ],
+];
--- app/resources/views/students/index.blade.php
+++ app/resources/views/students/index.blade.php
@@ -4,6 +4,22 @@
 
 <h1>Studenti.</h1>
 
-@dump($students)
+<div class="students">
+  @foreach($students as $student)
+    <a href="#" class="student">
+      <header>
+        <img src="{{ $student['img'] }}" alt="{{ $student['nome'] }}">
+        <div class="info">
+          <h3>{{ $student['nome'] }} ( {{ $student['eta'] }} anni)</h3>
+          <h4>Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}}</h4>
+        </div>{{-- info --}}
+      </header>
+      <p>
+        {{ $student['descrizione'] }}
+      </p>{{-- description --}}
+    </a>
+  @endforeach
+</div>
+
 
 @endsection
